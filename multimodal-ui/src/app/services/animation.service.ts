a8e2a4af38957e7a3887e68c96b3a510ccbaa7ea
Mahdi <gmx_mahdi@outlook.com>
2025-01-30 01:28:58 -0500

    add test animation

--- multimodal-ui/src/app/services/animation.service.ts
+++ multimodal-ui/src/app/services/animation.service.ts
@@ -2,33 +2,57 @@ import { Injectable } from '@angular/core';
 import * as PIXI from 'pixi.js';
 import * as L from 'leaflet';
 import 'leaflet-pixi-overlay';
+import { Entity } from '../interfaces/entity';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimationService {
-  private markers: PIXI.Sprite[] = [];
+  private ticker: PIXI.Ticker = new PIXI.Ticker();
+  private entities: Entity[] = [];
   private container = new PIXI.Container();
   private utils: L.PixiOverlayUtils;
 
-
+  private pointToReach: L.Point = new L.Point(45.523066, -73.652687);
 
   constructor() { 
   
   }
 
-  private addEntity(latlng: L.LatLng, type: string="sample-marker") {
-    let marker = PIXI.Sprite.from(`images/${type}.png`);
+  private addEntity(type: string="sample-marker") {
+    let sprite = PIXI.Sprite.from(`images/${type}.png`);
 
-    let coords = this.utils.latLngToLayerPoint(latlng);
-    marker.x = coords.x;
-    marker.y = coords.y;
-    marker.anchor.set(0.5, 0.5);
-    marker.scale.set(1 / this.utils.getScale());
 
-    this.container.addChild(marker);
-    this.markers.push(marker);
+    sprite.anchor.set(0.5, 1);
+    sprite.scale.set(1 / this.utils.getScale());
+    this.container.addChild(sprite);
+
+
+    let entity: Entity = {
+      sprite,
+      startPos: this.pointToReach,
+      endPos: this.pointToReach,
+      speed: Math.random() * 2 + 1,
+      currentTime: 0,
+      timeToReach: 5
+    }
+
+    this.entities.push(entity);
+  }
+
+  private changeEntitiesDestination(latlng: L.LatLng) {
+    this.pointToReach = this.utils.latLngToLayerPoint(latlng);
+    this.entities.forEach((entity) => {
+      entity.startPos.x = entity.sprite.x;
+      entity.startPos.y = entity.sprite.y;
+      entity.endPos = this.pointToReach;
+
+      let distanceVec = entity.endPos.subtract(entity.startPos);
+      let distance = Math.sqrt(distanceVec.x * distanceVec.x + distanceVec.y * distanceVec.y);
+      entity.timeToReach = distance * 0.5 / entity.speed;
+      entity.currentTime = 0;
+    })
   }
 
   // Called once when Pixi layer is added.
@@ -43,17 +67,24 @@ export class AnimationService {
   }
 
   private onRedraw(event: L.LeafletEvent) {
-    this.markers.forEach((marker) => {
-      marker.x += 0.05 * event.delta;
-      marker.y += 0.05 * event.delta;
+    this.entities.forEach((entity) => {
+      entity.currentTime += this.ticker.deltaTime / this.ticker.FPS;
+      let progress = entity.currentTime / entity.timeToReach;
+      if (progress >= 1) return;
+      let newPosition = entity.endPos.multiplyBy(progress).add(entity.startPos.multiplyBy(1 - progress));
+      entity.sprite.x = newPosition.x;
+      entity.sprite.y = newPosition.y;
     });
+  }
+
+  private onClick(event: L.LeafletMouseEvent) {
+    this.changeEntitiesDestination(event.latlng);
+    this.addEntity();
   }
 
 
   addPixiOverlay(map: L.Map) {
-    map.on('click', (e) => {
-      this.addEntity(e.latlng);
-    });
+    map.on('click', (event) => {this.onClick(event)});
 
     var pixiLayer = (() => {
       return L.pixiOverlay((utils, event) => {
@@ -69,10 +100,9 @@ export class AnimationService {
 
     pixiLayer.addTo(map);
 
-    let ticker = new PIXI.Ticker();
-    ticker.add(function (delta) {
+    this.ticker.add(function (delta) {
       pixiLayer.redraw({ type: 'redraw', delta: delta } as L.LeafletEvent);
     });
-    ticker.start();
+    this.ticker.start();
   }
 }
