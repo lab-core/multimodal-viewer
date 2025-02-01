9acaf9b11ccf4317fd53ddc15ac9a1170a9d26f3
Leo-Marbehan <90802811+Leo-Marbehan@users.noreply.github.com>
2025-02-01 17:14:47 -0500

    Information Panel and search bar

--- multimodal-ui/src/app/components/user-interface/user-interface.component.ts
+++ multimodal-ui/src/app/components/user-interface/user-interface.component.ts
@@ -1,9 +1,15 @@
 import { Component, Signal } from '@angular/core';
-import { UserInterfaceService } from '../../services/user-interface.service';
+import {
+  UserInterfaceService,
+  UserInterfaceView,
+} from '../../services/user-interface.service';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { firstValueFrom } from 'rxjs';
 import {
   SimulationConfigurationDialogComponent,
@@ -13,7 +19,13 @@ import {
 
 @Component({
   selector: 'app-user-interface',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+  ],
   templateUrl: './user-interface.component.html',
   styleUrl: './user-interface.component.css',
 })
@@ -22,8 +34,11 @@ export class UserInterfaceComponent {
     private readonly userInterfaceService: UserInterfaceService,
     private readonly matDialog: MatDialog
   ) {
-    this.userInterfaceService.showMainMenu();
-    this.userInterfaceService.dimMap();
+    this.userInterfaceService.navigateToMainMenu();
+  }
+
+  get currentViewSignal(): Signal<UserInterfaceView> {
+    return this.userInterfaceService.viewSignal;
   }
 
   get shouldShowMainMenuSignal(): Signal<boolean> {
@@ -32,6 +47,22 @@ export class UserInterfaceComponent {
 
   get shouldDimMapSignal(): Signal<boolean> {
     return this.userInterfaceService.shouldDimMapSignal;
+  }
+
+  get shouldShowSearchBarSignal(): Signal<boolean> {
+    return this.userInterfaceService.shouldShowSearchBarSignal;
+  }
+
+  get shouldShowInformationPanelSignal(): Signal<boolean> {
+    return this.userInterfaceService.shouldShowInformationPanelSignal;
+  }
+
+  hideInformationPanel() {
+    this.userInterfaceService.hideInformationPanel();
+  }
+
+  showInformationPanel() {
+    this.userInterfaceService.showInformationPanel();
   }
 
   async onStartSimulation() {
@@ -57,5 +88,8 @@ export class UserInterfaceComponent {
       this.userInterfaceService.showMainMenu();
       return;
     }
+
+    // TODO Start simulation
+    this.userInterfaceService.navigateToSimulation();
   }
 }
--- multimodal-ui/src/app/services/user-interface.service.ts
+++ multimodal-ui/src/app/services/user-interface.service.ts
@@ -1,26 +1,69 @@
-import { Injectable, Signal, signal, WritableSignal } from '@angular/core';
+import {
+  computed,
+  Injectable,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
-export type UserInterfaceView = 'main-menu' | 'start-simulation';
+export type UserInterfaceView = 'main-menu' | 'simulation';
 @Injectable({
   providedIn: 'root',
 })
 export class UserInterfaceService {
+  private readonly _viewSignal: WritableSignal<UserInterfaceView> =
+    signal<UserInterfaceView>('main-menu');
+
   private readonly _shouldShowMainMenuSignal: WritableSignal<boolean> =
     signal<boolean>(true);
   private readonly _shouldDimMapSignal: WritableSignal<boolean> =
     signal<boolean>(true);
 
+  private readonly _shouldShowSearchBarSignal: WritableSignal<boolean> =
+    signal<boolean>(false);
+  private readonly _shouldShowInformationPanelSignal: WritableSignal<boolean> =
+    signal<boolean>(false);
+
   constructor(private readonly matDialog: MatDialog) {}
 
+  // MARK: Signal Getters
+  get viewSignal(): Signal<UserInterfaceView> {
+    return this._viewSignal;
+  }
+
   get shouldShowMainMenuSignal(): Signal<boolean> {
-    return this._shouldShowMainMenuSignal;
+    return computed(() => {
+      return (
+        this._viewSignal() === 'main-menu' && this._shouldShowMainMenuSignal()
+      );
+    });
   }
 
   get shouldDimMapSignal(): Signal<boolean> {
-    return this._shouldDimMapSignal;
+    return computed(() => {
+      return this._viewSignal() === 'main-menu' && this._shouldDimMapSignal();
+    });
   }
 
+  get shouldShowSearchBarSignal(): Signal<boolean> {
+    return computed(() => {
+      return (
+        this._viewSignal() === 'simulation' && this._shouldShowSearchBarSignal()
+      );
+    });
+  }
+
+  get shouldShowInformationPanelSignal(): Signal<boolean> {
+    return computed(() => {
+      return (
+        this._viewSignal() === 'simulation' &&
+        this._shouldShowInformationPanelSignal()
+      );
+    });
+  }
+
+  // MARK: Signal Setters
   showMainMenu() {
     this._shouldShowMainMenuSignal.set(true);
   }
@@ -35,5 +78,38 @@ export class UserInterfaceService {
 
   unDimMap() {
     this._shouldDimMapSignal.set(false);
+  }
+
+  showSearchBar() {
+    this._shouldShowSearchBarSignal.set(true);
+  }
+
+  hideSearchBar() {
+    this._shouldShowSearchBarSignal.set(false);
+  }
+
+  showInformationPanel() {
+    this._shouldShowInformationPanelSignal.set(true);
+  }
+
+  hideInformationPanel() {
+    this._shouldShowInformationPanelSignal.set(false);
+  }
+
+  // MARK: Navigation
+  navigateToMainMenu() {
+    this._viewSignal.set('main-menu');
+    this.showMainMenu();
+    this.dimMap();
+    this.hideSearchBar();
+    this.hideInformationPanel();
+  }
+
+  navigateToSimulation() {
+    this._viewSignal.set('simulation');
+    this.hideMainMenu();
+    this.unDimMap();
+    this.showSearchBar();
+    this.showInformationPanel();
   }
 }
