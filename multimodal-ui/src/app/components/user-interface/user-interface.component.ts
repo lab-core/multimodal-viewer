cc16d4d97c9b5442106b5646e63e108d8e1dc8e0
Leo-Marbehan <90802811+Leo-Marbehan@users.noreply.github.com>
2025-02-01 16:40:02 -0500

    Configuration Dialog

--- multimodal-ui/src/app/components/user-interface/user-interface.component.ts
+++ multimodal-ui/src/app/components/user-interface/user-interface.component.ts
@@ -1,9 +1,15 @@
-import { Component, computed, Signal } from '@angular/core';
+import { Component, Signal } from '@angular/core';
 import { UserInterfaceService } from '../../services/user-interface.service';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { firstValueFrom } from 'rxjs';
+import {
+  SimulationConfigurationDialogComponent,
+  SimulationConfigurationDialogData,
+  SimulationConfigurationDialogResult,
+} from '../simulation-configuration-dialog/simulation-configuration-dialog.component';
 
 @Component({
   selector: 'app-user-interface',
@@ -15,13 +21,41 @@ export class UserInterfaceComponent {
   constructor(
     private readonly userInterfaceService: UserInterfaceService,
     private readonly matDialog: MatDialog
-  ) {}
+  ) {
+    this.userInterfaceService.showMainMenu();
+    this.userInterfaceService.dimMap();
+  }
 
   get shouldShowMainMenuSignal(): Signal<boolean> {
     return this.userInterfaceService.shouldShowMainMenuSignal;
   }
 
   get shouldDimMapSignal(): Signal<boolean> {
-    return computed(() => this.shouldShowMainMenuSignal());
+    return this.userInterfaceService.shouldDimMapSignal;
+  }
+
+  async onStartSimulation() {
+    this.userInterfaceService.hideMainMenu();
+
+    const result = await firstValueFrom(
+      this.matDialog
+        .open<
+          SimulationConfigurationDialogComponent,
+          SimulationConfigurationDialogData,
+          SimulationConfigurationDialogResult
+        >(SimulationConfigurationDialogComponent, {
+          data: { mode: 'start', currentConfiguration: null },
+          disableClose: true,
+          autoFocus: false,
+          maxWidth: '80vw',
+          maxHeight: '80vh',
+        })
+        .afterClosed()
+    );
+
+    if (!result) {
+      this.userInterfaceService.showMainMenu();
+      return;
+    }
   }
 }
--- multimodal-ui/src/app/services/user-interface.service.ts
+++ multimodal-ui/src/app/services/user-interface.service.ts
@@ -1,14 +1,24 @@
 import { Injectable, Signal, signal, WritableSignal } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
+export type UserInterfaceView = 'main-menu' | 'start-simulation';
 @Injectable({
   providedIn: 'root',
 })
 export class UserInterfaceService {
   private readonly _shouldShowMainMenuSignal: WritableSignal<boolean> =
-    signal(true);
+    signal<boolean>(true);
+  private readonly _shouldDimMapSignal: WritableSignal<boolean> =
+    signal<boolean>(true);
+
+  constructor(private readonly matDialog: MatDialog) {}
 
   get shouldShowMainMenuSignal(): Signal<boolean> {
     return this._shouldShowMainMenuSignal;
+  }
+
+  get shouldDimMapSignal(): Signal<boolean> {
+    return this._shouldDimMapSignal;
   }
 
   showMainMenu() {
@@ -17,5 +27,13 @@ export class UserInterfaceService {
 
   hideMainMenu() {
     this._shouldShowMainMenuSignal.set(false);
+  }
+
+  dimMap() {
+    this._shouldDimMapSignal.set(true);
+  }
+
+  unDimMap() {
+    this._shouldDimMapSignal.set(false);
   }
 }
