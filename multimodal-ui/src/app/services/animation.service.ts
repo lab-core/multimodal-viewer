dffaab0b19311ea484bf79a379482c21cc530449
Mahdi <gmx_mahdi@outlook.com>
2025-02-27 11:13:02 -0500

    fix linting

--- multimodal-ui/src/app/services/animation.service.ts
+++ multimodal-ui/src/app/services/animation.service.ts
@@ -1,11 +1,10 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { Injectable } from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet-pixi-overlay';
 import * as PIXI from 'pixi.js';
-import { Entity, EntityOwner, VehicleEntity } from '../interfaces/entity.model';
+import { EntityOwner, VehicleEntity } from '../interfaces/entity.model';
 import { SimulationEnvironment, Vehicle } from '../interfaces/simulation.model';
 import { Polylines } from '../interfaces/simulation.model';
-import { MapService } from './map.service';
 
 @Injectable({
   providedIn: 'root',
@@ -16,8 +15,8 @@ export class AnimationService {
   private readonly LIGHT_BLUE = 0xCDCDFF;
   private readonly SATURATED_RED = 0xCD2222;
 
-  private pause: boolean = false;
-  private animationVisualizationTime: number = 0;
+  private pause = false;
+  private animationVisualizationTime = 0;
 
   private ticker: PIXI.Ticker = new PIXI.Ticker();
   private vehicles: VehicleEntity[] = [];
@@ -25,10 +24,6 @@ export class AnimationService {
   private utils!: L.PixiOverlayUtils;
 
   private selectedVehicle: Vehicle | null = null;
-
-  constructor() {
-
-  }
 
   synchronizeEnvironment(simulationEnvironment: SimulationEnvironment) {
     console.log('Simulation env: ', simulationEnvironment);
@@ -141,13 +136,13 @@ export class AnimationService {
 
         // Calculate polyline progress
         /////////////////
-        let departureTime = vehicle.data.previousStops[polylineNo].departureTime ?? 0;
-        let arrivalTime = vehicle.data.nextStops[0].arrivalTime;
+        const departureTime = vehicle.data.previousStops[polylineNo].departureTime ?? 0;
+        const arrivalTime = vehicle.data.nextStops[0].arrivalTime;
         if (departureTime >= arrivalTime) {
           console.log('something went wrong...', vehicle)
           continue;
         };
-        let polylineProgress = (this.animationVisualizationTime - departureTime) / (arrivalTime - departureTime);
+        const polylineProgress = (this.animationVisualizationTime - departureTime) / (arrivalTime - departureTime);
         /////////////////
 
         polyline = vehicle.data.polylines[polylineNo];
@@ -158,11 +153,11 @@ export class AnimationService {
 
         // Find line number and line progress
         /////////////////
-        let coefficients = polyline.coefficients;
+        const coefficients = polyline.coefficients;
         let cummulativeProgress = 0;
         lineNo = 0;
         for (; lineNo < coefficients.length; ++lineNo) {
-          let nextCummulativeProgress = cummulativeProgress + coefficients[lineNo];
+          const nextCummulativeProgress = cummulativeProgress + coefficients[lineNo];
           if (polylineProgress < nextCummulativeProgress) {
             lineProgress = (polylineProgress - cummulativeProgress) / (nextCummulativeProgress - cummulativeProgress);
             break;
@@ -225,7 +220,7 @@ export class AnimationService {
   }
 
   private onClick(event: L.LeafletMouseEvent) {
-
+    // Do something.
   }
 
   private onEntityPointerdown(event: PIXI.FederatedPointerEvent) {
