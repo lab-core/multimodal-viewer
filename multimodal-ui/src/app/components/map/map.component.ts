d559c614c7dc460caf53e4409bb5066c5bfaa7c7
NumberFiveV <Vlad.Okuniev@yahoo.com>
2025-03-20 13:21:36 -0400

    Zoom saved to local storage

--- multimodal-ui/src/app/components/map/map.component.ts
+++ multimodal-ui/src/app/components/map/map.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject } from '@angular/core';
-
+import { Component, inject, OnDestroy } from '@angular/core';
 import { LeafletModule } from '@bluehalo/ngx-leaflet';
 import { latLng, Map, tileLayer } from 'leaflet';
 import { AnimationService } from '../../services/animation.service';
@@ -11,8 +10,9 @@ import { MapService } from '../../services/map.service';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css',
 })
-export class MapComponent {
+export class MapComponent implements OnDestroy {
   animationService: AnimationService = inject(AnimationService);
+  savedZoom = localStorage.getItem('mapZoom') ? parseInt(localStorage.getItem('mapZoom')!, 10) : 12;
 
   options = {
     layers: [
@@ -25,17 +25,36 @@ export class MapComponent {
       }),
     ],
 
-    // Montreal
-    zoom: 12,
+    zoom: this.savedZoom,
     center: latLng(45.523066, -73.652687),
   };
 
-  constructor(private readonly mapService: MapService) {}
+  private map!: Map;
+
+  constructor(private readonly mapService: MapService) {
+    window.addEventListener('beforeunload', this.saveZoomBeforeUnload.bind(this));
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('beforeunload', this.saveZoomBeforeUnload.bind(this));
+  }
 
   onMapReady(map: Map) {
+    this.map = map;
     this.mapService.map = map;
     map.attributionControl.setPosition('bottomleft');
     map.zoomControl.setPosition('bottomright');
     this.animationService.addPixiOverlay(map);
+
+    if (this.savedZoom) {
+      map.setZoom(this.savedZoom);
+    }
+  }
+
+  private saveZoomBeforeUnload() {
+    if (this.map) {
+      const currentZoom = this.map.getZoom();
+      localStorage.setItem('mapZoom', currentZoom.toString());
+    }
   }
 }
