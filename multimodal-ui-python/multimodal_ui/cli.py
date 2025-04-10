7d12a628a65429f6011818091979bf3b98f80c2e
NumberFiveV <Vlad.Okuniev@yahoo.com>
2025-04-10 12:45:48 -0400

    python package actually works

---
+++ multimodal-ui-python/multimodal_ui/cli.py
@@ -0,0 +1,18 @@
+import argparse
+from .server import serve_angular_app
+
+def main():
+    parser = argparse.ArgumentParser(description='Multimodal UI Application')
+    parser.add_argument('--port', type=int, default=8085, help='Port to serve the UI')
+    parser.add_argument('--backend-port', type=int, default=8090, 
+                       help='Port where backend server is running')
+    args = parser.parse_args()
+    
+    # Get the static files directory (relative to this package)
+    import os
+    static_dir = os.path.join(os.path.dirname(__file__), 'static')
+    
+    serve_angular_app(static_dir, args.port)
+
+if __name__ == '__main__':
+    main()
---
+++ multimodal-ui-python/multimodal_ui/server.py
@@ -0,0 +1,24 @@
+import http.server
+import socketserver
+import os
+import threading
+import webbrowser
+from functools import partial
+
+class AngularServer:
+    def __init__(self, static_dir, port=8085, backend_port=8090):
+        self.static_dir = os.path.abspath(static_dir)
+        self.port = port
+        self.backend_port = backend_port
+        self.handler = partial(http.server.SimpleHTTPRequestHandler, directory=self.static_dir)
+    
+    def start(self):
+        with socketserver.TCPServer(("", self.port), self.handler) as httpd:
+            print(f"Serving Angular app at http://localhost:{self.port}")
+            print(f"Backend is expected at http://localhost:{self.backend_port}")
+            webbrowser.open(f'http://localhost:{self.port}')
+            httpd.serve_forever()
+
+def serve_angular_app(static_dir, port=8085):
+    server = AngularServer(static_dir, port)
+    server.start()
