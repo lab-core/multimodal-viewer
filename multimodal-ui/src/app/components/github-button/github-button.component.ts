8eae8bb5704788a967d31a5ceba7d13abea70677
Leo-Marbehan <90802811+Leo-Marbehan@users.noreply.github.com>
2025-04-15 13:27:38 -0400

    Add github button

---
+++ multimodal-ui/src/app/components/github-button/github-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-github-button',
+  imports: [MatButtonModule],
+  templateUrl: './github-button.component.html',
+  styleUrl: './github-button.component.scss',
+})
+export class GithubButtonComponent {}
