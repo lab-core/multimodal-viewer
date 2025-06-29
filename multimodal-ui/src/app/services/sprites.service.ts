4deb19b89c6bffcbbb5d30e3ef74edfdfef02b9c
Leo-Marbehan <90802811+Leo-Marbehan@users.noreply.github.com>
2025-06-29 01:21:54 -0400

    background shape customization

--- multimodal-ui/src/app/services/sprites.service.ts
+++ multimodal-ui/src/app/services/sprites.service.ts
@@ -1,27 +1,55 @@
 import { Injectable } from '@angular/core';
+import { ColorSource } from '@pixi/color';
 import { Texture } from '@pixi/core';
+import { Graphics } from '@pixi/graphics';
 import * as L from 'leaflet';
 
-export type CustomTextureType =
+// MARK: Models
+export type CustomizationEntityType =
   | 'vehicle'
   | 'empty-stop'
-  | 'stop-with-passenger';
+  | 'stop-with-passenger'
+  | 'all';
 
-export const CUSTOM_TEXTURE_TYPES: CustomTextureType[] = [
+export const CUSTOMIZATION_ENTITY_TYPES: CustomizationEntityType[] = [
   'vehicle',
   'empty-stop',
   'stop-with-passenger',
+  'all',
 ];
 
-export type CustomTextureZoom = 'zoomed-in' | 'zoomed-out' | 'any';
+export type CUSTOMIZATION_ZOOM = 'zoomed-in' | 'zoomed-out' | 'any';
 
-export const CUSTOM_TEXTURE_ZOOMS: CustomTextureZoom[] = [
+export const CUSTOMIZATION_ZOOMS: CUSTOMIZATION_ZOOM[] = [
   'zoomed-in',
   'zoomed-out',
   'any',
 ];
 
-export interface CustomTexture {
+export type BackgroundShapeType =
+  | 'circle'
+  | 'square'
+  | 'triangle'
+  | 'pentagon'
+  | 'hexagon'
+  | 'octagon'
+  | 'star'
+  | 'cross'
+  | 'diamond';
+
+export const BACKGROUND_SHAPE_TYPES: BackgroundShapeType[] = [
+  'circle',
+  'square',
+  'triangle',
+  'pentagon',
+  'hexagon',
+  'octagon',
+  'star',
+  'cross',
+  'diamond',
+];
+
+export interface CustomizationFields {
   /**
    * The tags of the entity this texture is for.
    *
@@ -36,11 +64,22 @@ export interface CustomTexture {
    */
   mode: string | null;
 
-  type: CustomTextureType;
+  type: CustomizationEntityType;
 
-  zoom: CustomTextureZoom;
+  zoom: CUSTOMIZATION_ZOOM;
+}
 
+export interface CustomTexture extends CustomizationFields {
   url: string;
+}
+
+export interface BackgroundShape extends CustomizationFields {
+  shape: BackgroundShapeType;
+
+  /**
+   * The color of the background shape.
+   */
+  color: string;
 }
 
 export interface TextureSaveData {
@@ -57,6 +96,8 @@ export interface TextureSaveData {
 
   customTextures: CustomTexture[];
 
+  backgroundShapes: BackgroundShape[];
+
   colorPresetIndex: number;
   customColors: string[];
 }
@@ -65,8 +106,10 @@ export interface TextureSaveData {
   providedIn: 'root',
 })
 export class SpritesService {
-  readonly VERSION = 4;
+  // MARK: Properties
+  readonly VERSION = 5;
   readonly SPRITE_SIZE = 40; // px
+  readonly BACKGROUND_SHAPE_SIZE = 60; // px
 
   private readonly KEY_TEXTURES = 'multimodal.textures';
 
@@ -121,6 +164,8 @@ export class SpritesService {
 
   private _customTextures: CustomTexture[] = [];
 
+  private _backgroundShapes: BackgroundShape[] = [];
+
   private _colorPresetIndex = 0;
 
   private _customColors: string[] = ['#00ff00', '#ff0000']; // Default custom preset
@@ -129,7 +174,7 @@ export class SpritesService {
 
   private _textureMap = new Map<string, Texture>();
 
-  // Getters
+  // MARK: Getters
   get useZoomedOutSprites(): boolean {
     return this._useZoomedOutSprites;
   }
@@ -182,10 +227,16 @@ export class SpritesService {
     return structuredClone(this._customTextures);
   }
 
+  get backgroundShapes(): BackgroundShape[] {
+    return structuredClone(this._backgroundShapes);
+  }
+
+  // MARK: Constructor
   constructor() {
     this.loadTexturesData();
   }
 
+  // MARK: Utilities
   calculateSpriteScales(utils: L.PixiOverlayUtils) {
     const MAX_SPRITE_SCALE = 1;
     const MIN_SPRITE_SCALE = 0.2;
@@ -220,6 +271,7 @@ export class SpritesService {
     zoomedOutStopWithPassengerTextureUrl: string,
     zoomedOutEmptyStopTextureUrl: string,
     customTextures: CustomTexture[],
+    backgroundShapes: BackgroundShape[],
     colorPresetIndex: number,
     customColors: string[],
   ) {
@@ -232,6 +284,7 @@ export class SpritesService {
       zoomedOutStopWithPassengerTextureUrl,
       zoomedOutEmptyStopTextureUrl,
       customTextures,
+      backgroundShapes,
       colorPresetIndex,
       customColors,
     };
@@ -241,12 +294,13 @@ export class SpritesService {
     this.applyTexturesData(saveData);
   }
 
-  getVehicleTexture(mode: string | null, tags: string[]) {
+  // MARK: Textures
+  getVehicleTexture(mode: string | null, tags: string[]): Texture {
     const currentZoom = this._useZoomedOutSprites ? 'zoomed-out' : 'zoomed-in';
 
     const firstMatchingTexture = this._customTextures.find(
       (texture) =>
-        texture.type === 'vehicle' &&
+        ['vehicle', 'all'].includes(texture.type) &&
         [currentZoom, 'any'].includes(texture.zoom) &&
         texture.tags.every((tag) => tags.includes(tag)) &&
         (texture.mode === null || texture.mode === mode),
@@ -264,13 +318,16 @@ export class SpritesService {
   /**
    * @deprecated passenger are not displayed anymore, stops are used instead.
    */
-  getCurrentPassengerTexture() {
+  getCurrentPassengerTexture(): Texture {
     return this._useZoomedOutSprites
       ? this._zoomedOutStopWithPassengerTexture
       : this._stopWithPassengerTexture;
   }
 
-  getStopWithPassengerTexture(tags: string[], passengerTags: string[]) {
+  getStopWithPassengerTexture(
+    tags: string[],
+    passengerTags: string[],
+  ): Texture {
     const currentZoom = this._useZoomedOutSprites ? 'zoomed-out' : 'zoomed-in';
 
     // If the stop has no tag, we use the passenger tags instead.
@@ -278,7 +335,7 @@ export class SpritesService {
 
     const firstMatchingTexture = this._customTextures.find(
       (texture) =>
-        texture.type === 'stop-with-passenger' &&
+        ['stop-with-passenger', 'all'].includes(texture.type) &&
         [currentZoom, 'any'].includes(texture.zoom) &&
         texture.tags.every((tag) => usedTags.includes(tag)),
     );
@@ -292,12 +349,12 @@ export class SpritesService {
     );
   }
 
-  getEmptyStopTexture(tags: string[]) {
+  getEmptyStopTexture(tags: string[]): Texture {
     const currentZoom = this._useZoomedOutSprites ? 'zoomed-out' : 'zoomed-in';
 
     const firstMatchingTexture = this._customTextures.find(
       (texture) =>
-        texture.type === 'empty-stop' &&
+        ['empty-stop', 'all'].includes(texture.type) &&
         [currentZoom, 'any'].includes(texture.zoom) &&
         texture.tags.every((tag) => tags.includes(tag)),
     );
@@ -311,6 +368,86 @@ export class SpritesService {
     );
   }
 
+  // MARK: Background shapes
+  drawVehicleBackgroundShape(
+    graphics: Graphics,
+    mode: string | null,
+    tags: string[],
+  ): void {
+    const currentZoom = this._useZoomedOutSprites ? 'zoomed-out' : 'zoomed-in';
+
+    const firstMatchingBackgroundShape = this._backgroundShapes.find(
+      (backgroundShape) =>
+        ['vehicle', 'all'].includes(backgroundShape.type) &&
+        [currentZoom, 'any'].includes(backgroundShape.zoom) &&
+        backgroundShape.tags.every((tag) => tags.includes(tag)) &&
+        (backgroundShape.mode === null || backgroundShape.mode === mode),
+    );
+
+    if (!firstMatchingBackgroundShape) {
+      graphics.clear();
+      return;
+    }
+
+    this.drawShape(
+      graphics,
+      firstMatchingBackgroundShape.shape,
+      firstMatchingBackgroundShape.color,
+    );
+  }
+
+  drawStopWithPassengerBackgroundShape(
+    graphics: Graphics,
+    tags: string[],
+    passengerTags: string[],
+  ): void {
+    const currentZoom = this._useZoomedOutSprites ? 'zoomed-out' : 'zoomed-in';
+
+    // If the stop has no tag, we use the passenger tags instead.
+    const usedTags = tags.length > 0 ? tags : passengerTags;
+
+    const firstMatchingBackgroundShape = this._backgroundShapes.find(
+      (backgroundShape) =>
+        ['stop-with-passenger', 'all'].includes(backgroundShape.type) &&
+        [currentZoom, 'any'].includes(backgroundShape.zoom) &&
+        backgroundShape.tags.every((tag) => usedTags.includes(tag)),
+    );
+
+    if (!firstMatchingBackgroundShape) {
+      graphics.clear();
+      return;
+    }
+
+    this.drawShape(
+      graphics,
+      firstMatchingBackgroundShape.shape,
+      firstMatchingBackgroundShape.color,
+    );
+  }
+
+  drawEmptyStopBackgroundShape(graphics: Graphics, tags: string[]) {
+    const currentZoom = this._useZoomedOutSprites ? 'zoomed-out' : 'zoomed-in';
+
+    const firstMatchingBackgroundShape = this._backgroundShapes.find(
+      (backgroundShape) =>
+        ['empty-stop', 'all'].includes(backgroundShape.type) &&
+        [currentZoom, 'any'].includes(backgroundShape.zoom) &&
+        backgroundShape.tags.every((tag) => tags.includes(tag)),
+    );
+
+    if (!firstMatchingBackgroundShape) {
+      graphics.clear();
+      return;
+    }
+
+    this.drawShape(
+      graphics,
+      firstMatchingBackgroundShape.shape,
+      firstMatchingBackgroundShape.color,
+    );
+  }
+
+  // MARK: Private utilities
   private loadTexturesData() {
     const savedTexturesJson = localStorage.getItem(this.KEY_TEXTURES);
     if (!savedTexturesJson) return;
@@ -370,5 +507,102 @@ export class SpritesService {
     urls.forEach((url) => {
       this._textureMap.set(url, Texture.from(url));
     });
+
+    // Create background shapes
+    this._backgroundShapes = structuredClone(textureSaveData.backgroundShapes);
+  }
+
+  private drawShape(
+    graphics: Graphics,
+    shape: BackgroundShapeType,
+    color: ColorSource,
+  ) {
+    const size = this.BACKGROUND_SHAPE_SIZE;
+
+    graphics.clear();
+    graphics.beginFill(color);
+
+    switch (shape) {
+      case 'circle':
+        graphics.drawCircle(0, 0, size / 2);
+        graphics.scale.set(4 / Math.PI);
+        break;
+      case 'square':
+        graphics.drawRect(-size / 2, -size / 2, size, size);
+        break;
+      case 'triangle':
+        graphics.drawPolygon(this.createPolygonCoordinates(3, size / 2));
+        graphics.scale.set(1.5);
+        break;
+      case 'pentagon':
+        graphics.drawPolygon(this.createPolygonCoordinates(5, size / 2));
+        graphics.scale.set(4 / Math.PI);
+        break;
+      case 'hexagon':
+        graphics.drawPolygon(this.createPolygonCoordinates(6, size / 2, 0));
+        graphics.scale.set(4 / Math.PI);
+        break;
+      case 'octagon':
+        graphics.drawPolygon(
+          this.createPolygonCoordinates(8, size / 2, 360 / 16),
+        );
+        graphics.scale.set(4 / Math.PI);
+        break;
+      case 'star':
+        {
+          const outerRadius = size / 2;
+          const innerRadius = outerRadius / 2.5; // Adjust inner radius for star
+
+          const outerPolygon = this.createPolygonCoordinates(
+            5,
+            outerRadius,
+            90,
+          );
+
+          const innerPolygon = this.createPolygonCoordinates(
+            5,
+            innerRadius,
+            90 + 36, // Offset by 36 degrees for star points
+          );
+
+          const starCoordinates = [];
+          for (let i = 0; i < outerPolygon.length; i += 2) {
+            starCoordinates.push(innerPolygon[i], innerPolygon[i + 1]);
+            starCoordinates.push(outerPolygon[i], outerPolygon[i + 1]);
+          }
+
+          graphics.drawPolygon(starCoordinates);
+          graphics.scale.set(1.8);
+        }
+        break;
+      case 'cross':
+        graphics.drawRect(-size / 2, -size / 8, size, size / 4);
+        graphics.drawRect(-size / 8, -size / 2, size / 4, size);
+        graphics.rotation = Math.PI / 4; // Rotate to make it a cross
+        graphics.scale.set(1.5);
+        break;
+      case 'diamond':
+        graphics.drawPolygon(this.createPolygonCoordinates(4, size / 2));
+        graphics.scale.set(Math.SQRT2);
+        break;
+    }
+
+    graphics.endFill();
+  }
+
+  private createPolygonCoordinates(
+    numberOfPoints: number,
+    radius: number,
+    startAngle = 90, // In degrees, starting at 90 degrees
+  ): number[] {
+    const coordinates: number[] = [];
+    for (let i = 0; i < numberOfPoints; i++) {
+      // Start at 90 degrees and go clockwise
+      const angle = ((i * 360) / numberOfPoints - startAngle) * (Math.PI / 180);
+      const x = radius * Math.cos(angle);
+      const y = radius * Math.sin(angle);
+      coordinates.push(x, y);
+    }
+    return coordinates;
   }
 }
