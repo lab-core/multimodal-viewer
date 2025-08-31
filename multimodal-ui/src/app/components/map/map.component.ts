22aec735cc48128c6c4c3e953033ac0d3a6d16f4
Leo-Marbehan <90802811+Leo-Marbehan@users.noreply.github.com>
2025-08-31 17:35:36 -0400

    clean up and keep default tiles

--- multimodal-ui/src/app/components/map/map.component.ts
+++ multimodal-ui/src/app/components/map/map.component.ts
@@ -1,6 +1,10 @@
-import { Component, inject, OnDestroy } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { LeafletModule } from '@bluehalo/ngx-leaflet';
-import { latLng, LatLngExpression, Map } from 'leaflet';
+import { latLng, Map } from 'leaflet';
+import {
+  MAP_CENTER_LOCAL_STORAGE_KEY,
+  MAP_ZOOM_LOCAL_STORAGE_KEY,
+} from '../../interfaces/local-storage';
 import { AnimationService } from '../../services/animation.service';
 import { MapService } from '../../services/map.service';
 import { CloseEntitiesMenuComponent } from '../close-entities-menu/close-entities-menu.component';
@@ -12,26 +16,12 @@ import { CloseEntitiesMenuComponent } from '../close-entities-menu/close-entitie
   styleUrl: './map.component.css',
 })
 export class MapComponent implements OnDestroy {
-  animationService: AnimationService = inject(AnimationService);
+  private map: Map | null = null;
 
-  // Retrieve saved zoom and position from localStorage
-  savedZoom = localStorage.getItem('mapZoom')
-    ? parseInt(localStorage.getItem('mapZoom')!, 10)
-    : 12;
-  savedCenter: LatLngExpression = localStorage.getItem('mapCenter')
-    ? (JSON.parse(localStorage.getItem('mapCenter')!) as [number, number])
-    : latLng(45.523066, -73.652687); // Montreal as Default
-
-  options = {
-    layers: [],
-
-    zoom: this.savedZoom,
-    center: this.savedCenter,
-  };
-
-  private map!: Map;
-
-  constructor(private readonly mapService: MapService) {
+  constructor(
+    private readonly mapService: MapService,
+    private readonly animationService: AnimationService,
+  ) {
     window.addEventListener('beforeunload', this.saveMapState.bind(this));
   }
 
@@ -43,30 +33,36 @@ export class MapComponent implements OnDestroy {
   onMapReady(map: Map) {
     this.map = map;
     this.mapService.map = map;
-    this.mapService.selectMapTile(this.mapService.selectedMapTile());
+    this.mapService.selectMapTile(this.mapService.selectedMapTileSignal());
 
     map.attributionControl.setPosition('bottomleft');
     map.zoomControl.setPosition('bottomright');
+
     this.animationService.addPixiOverlay(map);
 
-    if (this.savedZoom) {
-      map.setZoom(this.savedZoom);
-    }
-    if (this.savedCenter) {
-      map.setView(this.savedCenter, this.savedZoom);
-    }
+    const savedCenter = localStorage.getItem(MAP_CENTER_LOCAL_STORAGE_KEY);
+    const center = savedCenter
+      ? (JSON.parse(savedCenter) as [number, number])
+      : latLng(45.523066, -73.652687); // Montreal as Default
+
+    const savedZoom = localStorage.getItem(MAP_ZOOM_LOCAL_STORAGE_KEY);
+    const zoom = savedZoom ? parseInt(savedZoom, 10) : 12;
+
+    map.setView(center, zoom);
   }
 
   private saveMapState() {
-    if (this.map) {
-      const currentZoom = this.map.getZoom();
-      const currentCenter = this.map.getCenter();
-
-      localStorage.setItem('mapZoom', currentZoom.toString());
-      localStorage.setItem(
-        'mapCenter',
-        JSON.stringify([currentCenter.lat, currentCenter.lng]),
-      );
+    if (!this.map) {
+      return;
     }
+
+    const currentZoom = this.map.getZoom();
+    const currentCenter = this.map.getCenter();
+
+    localStorage.setItem(MAP_ZOOM_LOCAL_STORAGE_KEY, currentZoom.toString());
+    localStorage.setItem(
+      MAP_CENTER_LOCAL_STORAGE_KEY,
+      JSON.stringify([currentCenter.lat, currentCenter.lng]),
+    );
   }
 }
---
+++ multimodal-ui/src/app/interfaces/local-storage.ts
@@ -0,0 +1,6 @@
+export const LOCAL_STORAGE_KEY = 'multimodal-viewer';
+
+export const MAP_LOCAL_STORAGE_KEY = LOCAL_STORAGE_KEY + '.map';
+
+export const MAP_CENTER_LOCAL_STORAGE_KEY = MAP_LOCAL_STORAGE_KEY + '.center';
+export const MAP_ZOOM_LOCAL_STORAGE_KEY = MAP_LOCAL_STORAGE_KEY + '.zoom';
